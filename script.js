fc9f8f60a03237d7330ac49d75602c621c39013d
Quan Truong <truonghoangquan456@gmail.com>
2025-03-08 01:40:45 -0500

    Finish implement all of the features
    All functions of the calculator have been implemented. So far, no
    bugs found. I will move on to the next phase, which is CSS
    for the calculator

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let multiply = (a, b) => a * b;
 let divide = (a, b) => a / b;
-let num = NaN;
+let num = 0;
 // let secondNumber = NaN;
 let lastOp = ""; 
 let newInput = false;
+let haveDecimal = false;
+let operationClicked = false;
 
 function operate(a, b, op) {
     a = +a;
@@ -17,18 +19,29 @@ function operate(a, b, op) {
 }
 
 function modifyDisplay(char) { //we want to add char to display
+    if (char == ".") {
+        if (haveDecimal) return;
+        haveDecimal = true;
+    }
+    if (newInput) {
+        display.textContent = ""; 
+        newInput = false;
+    }
     display.textContent += char;
+    if (display.textContent.length > 9) {
+        display.textContent = +(+display.textContent).toFixed(8);
+    }
+    while(display.textContent.charAt(0) == "0" && display.textContent.length > 1 && display.textContent.charAt(1) == "0") {
+        display.textContent = display.textContent.slice(1);
+    }
 }
 
 let digitClicked = -1; 
 for(let i = 0; i <= 9; ++i) {
     let btn = document.getElementById(`digit-${i}`);
     btn.addEventListener( "click", ()=> {
-        if (newInput) {
-            display.textContent = ""; 
-            newInput = false;
-        }
-        display.textContent += i; 
+        modifyDisplay(i);
+        operationClicked = false;
     });
 }
 
@@ -36,19 +49,49 @@ let display = document.getElementById("display");
 let clear = document.getElementById("clear"); 
 clear.addEventListener("click", ()=>{
     display.textContent = "";
+    lastOp = "";
+    newInput = false;
+    haveDecimal = false;
+    num = 0;
+    operationClicked = false;
 });
 
 let operations = [...document.querySelectorAll(".operation")];
 for(let operation of operations) {
     operation.addEventListener( "click", ()=>{
-        if (Number.isNaN(num)) {
-            num = +display.textContent; 
+        if (!operationClicked) {
+            if (lastOp == "") {
+                num = +display.textContent; 
+            }
+            else {
+                num = operate(num, display.textContent, lastOp);
+                newInput = true;
+                modifyDisplay(num, newInput);
+            }
         }
-        else {
-            num = operate(num, display.textContent, lastOp);
-            display.textContent = num;
-        }
+        operationClicked = true;
         newInput = true;
-        lastOp = operation.textContent; 
+        lastOp = operation.textContent;
+        haveDecimal = false; 
     })
 }
+
+let decimal = document.getElementById("decimal");
+decimal.addEventListener("click", ()=>{
+    modifyDisplay(".");
+})
+
+let equal = document.getElementById("equal"); 
+equal.addEventListener("click", ()=>{
+    if (lastOp != "") {
+        if (!operationClicked) {
+            num = operate(num, display.textContent, lastOp);
+            newInput = true;
+            modifyDisplay(num);
+        }
+        lastOp = ""; 
+        newInput = true;
+        haveDecimal = false;
+        operationClicked = false;
+    }
+});
